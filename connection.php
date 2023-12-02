287b0beef2affb6e457a9250e27bb39d50382d53
prajeet1000 <132644038+prajeet1000@users.noreply.github.com>
2023-12-02 23:17:44 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 	error_reporting(0);
 	session_start();
-	$servername = "10.106.18.114";
+	$servername = "localhost";
 	$username 	= "root";
-	$password 	= "";
+	$password 	= "password";
 	$db_name 	= "inventory_system";
 	$conn 		= new mysqli($servername, $username, $password,$db_name);
 	if ($conn->connect_error) {
@@ -13,5 +13,5 @@
 	header("Content-Type: text/html; charset=utf-8");
 	mysqli_set_charset($conn,"utf8");
 		
-	define('SITE_URL','http://10.106.18.114/inventory-system/');
+	define('SITE_URL','http://localhost/inventory-system/');
 ?>
